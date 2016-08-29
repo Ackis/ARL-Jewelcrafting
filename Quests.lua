af229c742dd3f415c0b0b6914b11986993d272e2
Teresa <resa1983@hotmail.com>
2016-08-29 14:16:26 -0400

    To add quests

--- Quests.lua
+++ Quests.lua
@@ -45,10 +45,21 @@ function module:InitializeQuests()
 	AddQuest(40534,		Z.DALARAN_BROKENISLES,	39.6,	34.8,	"Neutral")
 	AddQuest(40536,		Z.VALSHARAH,		45.2,	73.4,	"Neutral") --Needs updating
 	AddQuest(40538,		Z.DALARAN_BROKENISLES,	39.6,	34.8,	"Neutral")
+	AddQuest(40539,		Z.DALARAN_BROKENISLES,	39.6,	34.8,	"Neutral") --Needs updating
 	AddQuest(40542,		Z.DALARAN_BROKENISLES,	39.6,	34.8,	"Neutral") --Needs updating
 	AddQuest(40543,		Z.DALARAN_BROKENISLES,	39.6,	34.8,	"Neutral") --Needs updating
 	AddQuest(40544,		Z.DALARAN_BROKENISLES,	39.6,	34.8,	"Neutral") --Needs updating
-
+	AddQuest(40546,		Z.STORMHEIM,		0.0,	0.0,	"Neutral") --Needs updating
+	AddQuest(40558,		Z.DALARAN_BROKENISLES,	39.6,	34.8,	"Neutral") --Needs updating
+	AddQuest(40559,		Z.DALARAN_BROKENISLES,	39.6,	34.8,	"Neutral")
+	AddQuest(40560,		Z.DALARAN_BROKENISLES,	39.6,	34.8,	"Neutral")
+	AddQuest(40561,		Z.DALARAN_BROKENISLES,	39.6,	34.8,	"Neutral")
+	AddQuest(41652,		Z.VALSHARAH,		54.6,	73.2,	"Neutral")
+	AddQuest(41651,		Z.AZSUNA,		46.8,	41.4,	"Neutral")
+	AddQuest(41653,		Z.THUNDER_TOTEM,	38.4,	46.0,	"Neutral")
+	AddQuest(41654,		Z.STORMHEIM,		 0.0,	 0.0,	"Neutral") --Needs updating
+	AddQuest(41656,		Z.DALARAN_BROKENISLES,	33.4,	18.0,	"Neutral")
+	AddQuest(43943,		Z.SURAMAR,		0.0,	0.0,	"Neutral") --Needs updating
 
 	self.InitializeQuests = nil
 end
