31e37683bca82da0bfd342625481d5d6808d919e
Teresa <resa1983@hotmail.com>
2016-08-29 14:16:18 -0400

    Add mobs

--- MobDrops.lua
+++ MobDrops.lua
@@ -64,6 +64,10 @@ function module:InitializeMobDrops()
 	AddMob(76266,	BN.HIGH_SAGE_VIRYX,				Z.SKYREACH)
 	AddMob(87493,	BN.RUKHMAR,					Z.SPIRES_OF_ARAK,				35.0, 32.0)
 	AddMob(95067,	BN.SHADOW_LORD_ISKAR,				Z.HELLFIRE_CITADEL)
+	AddMob(99611,	BN.THE_AMALGAM_OF_SOULS,			Z.BLACK_ROOK_HOLD,				 0.0,  0.0)
+	AddMob(107704,	BN.DARGRUL_THE_UNDERKING,			Z.NELTHARIONS_LAIR,				17.0, 70.0)
+	AddMob(109331,	BN.CALAMIR,					Z.SURAMAR,					 0.0,  0.0)
+	AddMob(110962,	BN.ODYN,					Z.HALLS_OF_VALOR,				 0.0,  0.0)
 
 	self.InitializeMobDrops = nil
 end
